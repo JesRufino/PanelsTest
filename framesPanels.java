4f36f22d70e1adf4f527c8910b8aa2e311465fb1
Jesimiel Rufino <jesrufino@gmail.com>
2016-08-10 01:02:48 -0300

    Mais teste de commit no Panel

---
+++ framesPanels.java
@@ -0,0 +1,43 @@
+import javax.swing.JFrame;
+
+public class framesPanels {
+
+	public static void main(String[] args) {
+		JFrame janela = new JFrame("Meu primeiro frame em Java");
+		Painel meuPainel = new Painel();
+		
+		janela.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		janela.add(meuPainel);
+		janela.setSize(600,400);
+		janela.setVisible(true);
+	}
+
+}
+
+Painel.java
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+public class Painel extends JPanel{
+	public void paintComponent( Graphics g ){
+	    super.paintComponent( g );
+	    int pixel=0;
+	    
+	    for(pixel=0 ; pixel <= getHeight() ; pixel += 10){
+	        g.drawLine(0, pixel, pixel, getHeight());
+	        }
+
+	    for(pixel=getHeight() ; pixel >=0 ; pixel -= 10){
+	        g.drawLine(0, pixel, getHeight() - pixel, 0);
+	    }
+	    
+	    for(pixel=0 ; pixel <= getHeight() ; pixel +=10){
+	        g.drawLine(getWidth(), pixel, getWidth() - pixel, getHeight());
+	    }
+	    
+	    for(pixel=getHeight() ; pixel >=0 ; pixel -= 10){
+	        g.drawLine(getWidth(), pixel, getWidth() - (getHeight() - pixel), 0);
+	    }
+	    
+	}
+}
